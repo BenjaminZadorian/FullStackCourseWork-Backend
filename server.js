82d42babe312741f465ca299a21f98964372a027
BZ087 <BZ087@live.mdx.ac.uk>
2025-10-29 19:39:21 +0000

    Basic setup of express + mongoDB connection

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+// setup the environmental vars
+dotenv.config();
+
+// setup basic app config
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+// setup server port
+// access the .env file port variable or just use 5000 as default
+const port = process.env.PORT || 5000
+
+// create a mongodb instance of the client using the connection string from the .env file
+const client = new MongoClient(process.env.MONGODB_URI);
+
+// function to connect to the database
+async function connectDB() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB Database");
+    } catch (error) {
+        console.error("Database connection failed: ", error);
+    }
+}
+
+connectDB();
+
+// test route
+app.get(`/`, (req, res) => {
+    res.send("Backend is connected");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
