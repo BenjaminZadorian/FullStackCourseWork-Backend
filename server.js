ffc3f2abf9deae08c96fb931d58f0ac64fb99ece
BZ087 <BZ087@live.mdx.ac.uk>
2025-10-31 14:14:25 +0000

    Changed .env -> db.properties and downloaded PropertiesReader

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 // import routes
 import classesRouter from "./routes/classes.js"
-// setup the environmental vars
-dotenv.config();
+import PropertiesReader from "properties-reader";
+
+// load db.properties
+const properties = PropertiesReader("db.properties");
 
 // setup basic app config
 const app = express();
@@ -16,10 +18,11 @@ app.use(`/api/classes`, classesRouter);
 
 // setup server port
 // access the .env file port variable or just use 5000 as default
-const port = process.env.PORT || 5000
+const port = properties.get("PORT") || 5000;
+const mongoUri = properties.get("MONGODB_URI");
 
 // create a mongodb instance of the client using the connection string from the .env file
-const client = new MongoClient(process.env.MONGODB_URI);
+const client = new MongoClient(mongoUri);
 
 // function to connect to the database
 async function connectDB() {
