bacdc20cbed10da2a7c27167a42bd897b989c836
BZ087 <BZ087@live.mdx.ac.uk>
2025-11-09 17:45:50 +0000

    Updated CORS to hopeful fix registration issue

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ import registerRouter from "./routes/register.js";
 const PORT = process.env.PORT || 5000 ;
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
 
