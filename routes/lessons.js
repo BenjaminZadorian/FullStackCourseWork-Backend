846daae7e10bdcc94bffa5e1bd638fbf6008c584
BZ087 <BZ087@live.mdx.ac.uk>
2025-11-13 18:44:29 +0000

    Removed anything to do with the PUT for orders, to get a fresh start

--- routes/lessons.js
+++ routes/lessons.js
@@ -1,5 +1,4 @@
 import express from "express";
-import {ObjectId} from "mongodb"
 
 // create a function to export that controls database calls from this route
 export default function lessonsRouter(db) {
@@ -19,43 +18,6 @@ export default function lessonsRouter(db) {
   });
 
   // PUT: update spaces after an order is confirmed
-  router.put(`/:id`, async(req, res) => {
-    try {
-
-      const lessonId = req.params.id;
-      const updateData = req.body;
-
-      if (!ObjectId.isValid(lessonId)) {
-        return res.status(400).json({message: "Invalid lesson iD"});
-      }
-
-      if (!updateData || Object.keys(updateData).length === 0) {
-        return res.status(400).json({ message: "No update data provided" });
-      }
-
-            let query;
-            if (ObjectId.isValid(lessonId)) {
-              query = { _id: new ObjectId(lessonId) };
-            } else {
-              query = { _id: lessonId };
-            }
-
-      const data = await lessonsCollection.updateOne(
-        { _id: new ObjectId(lessonId) },
-        { $set: updateData }
-      );
-
-      if (data.matchedCount === 0) {
-        return res.status(404).json({ message: "Lesson not found" });
-      }
-
-      res.status(200).json({ message: "Lesson updated" });
-
-    } catch (error) {
-      console.error("Error fetching lessons:", error);
-      res.status(500).json({ message: "Error retrieving lessons" });
-    }
-  })
 
   return router;
 }
