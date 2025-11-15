86bee10810bf9ff57d3ae1548e60cc336a194d19
BZ087 <BZ087@live.mdx.ac.uk>
2025-11-15 12:14:21 +0000

    Redone PUT route for easier routing

--- routes/lessons.js
+++ routes/lessons.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { ObjectId } from "mongodb";
 
 // create a function to export that controls database calls from this route
 export default function lessonsRouter(db) {
@@ -18,6 +19,34 @@ export default function lessonsRouter(db) {
   });
 
   // PUT: update spaces after an order is confirmed
+  router.put(`/:id`, async (req, res) => {
+    try {
+
+      const lessonId = req.params.id;
+      const { spaces } = req.body;
+
+      if (!spaces) {
+        return res.status(400).json({message: "Invalid number of spaces"});
+      }
+
+      const objectId = newObjectId(lessonId);
+      const result = await lessonsCollection.updateOne(
+        { _id: objectId},
+        { $set: {spaces: spaces}}
+      );
+
+      if (result.matchedCount === 0) {
+        return res.status(404).json({message: "lesson not found"});
+      }
+
+      res.json({ message: "Lesson updated successfully" });
+
+    } catch (error) {
+      console.error("Error updating lessons:", error);
+      res.status(500).json({ message: "Error updating lessons" });
+    }
+  })
+
 
   return router;
 }
